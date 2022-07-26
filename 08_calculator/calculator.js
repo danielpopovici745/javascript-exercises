d9fadc20b45f763135b3db2aca174524d6c86852
Daniel Popovici <dpopovici601@gmail.com>
2022-07-26 12:51:31 -0400

    completed exercise 8 which required me to create a calculator.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
+const add = function(num1,num2) {
+  return num1+num2;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let total = 0;
+	arr.forEach(item => total += item);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let total = 1;
+  arr.forEach(item => total *= item );
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1,num2) {
+	return Math.pow(num1,num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let facto = 1;
+  if (num <=1){
+    return facto;
+  }
+  for (;num >1; num--){
+    facto *= num;
+  }
+  return facto;
 };
 
 // Do not edit below this line
