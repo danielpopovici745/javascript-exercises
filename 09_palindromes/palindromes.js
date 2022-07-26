a06dbfd386fb67ea05d8dcfd6d14c6d07bcb59c3
Daniel Popovici <dpopovici601@gmail.com>
2022-07-26 13:58:38 -0400

    finished exercise 8 which tests if a string is a palindrome. had to use regex for the first time and it was difficult but i was able to comprehend what is going on.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const onlyLettersArray = str
+        .toLowerCase()
+        .split('')
+        .filter(char => /[a-zA-Z]/.test(char));
+    console.log(onlyLettersArray);
+    let onlyLettersString = onlyLettersArray.join('');
+    const reversed = onlyLettersArray.reverse();
+    console.log(reversed);
+    let newStr = reversed.join('');
+    return newStr.includes(onlyLettersString);
 };
 
 // Do not edit below this line
