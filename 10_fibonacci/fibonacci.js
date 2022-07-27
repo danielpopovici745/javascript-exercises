fe5ac7b603553329b9f457c97ffe9276cde63652
Daniel Popovici <dpopovici601@gmail.com>
2022-07-27 10:33:15 -0400

    finsihed fibonacci exercise. created fibonacci sequence usign a for loop, could also be done with Binet's Fibonacci Number Formula.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(element) {
+    if (element <0) return "OOPS"
+    if(typeof element === 'string'){
+        element = parseInt(element);
+    }
+    let n1 = 1;
+    let n2= 1;
+    let sum;
+    for(let i = 1; i<element;i++){
+        sum = n1 + n2;
+        n1 = n2;
+        n2 = sum;
+    }
+    return n1;
 
+    //can also use Binet's Fibonacci Number Formula
 };
 
 // Do not edit below this line
